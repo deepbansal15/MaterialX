431368e76a740d5fc2a12f8b780ec2ebd3b590bc
Deep Bansal <deepbansal@live.com>
2025-12-27 19:55:13 +0000

    Add backward compatability SurfaceNode for HLSL

---
+++ source/MaterialXGenHlsl/Nodes/SurfaceNodeHlsl.h
@@ -0,0 +1,19 @@
+//
+// Copyright Contributors to the MaterialX Project
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#ifndef MATERIALX_SURFACENODEHLSL_H
+#define MATERIALX_SURFACENODEHLSL_H
+
+#include <MaterialXGenHw/Nodes/HwSurfaceNode.h>
+
+MATERIALX_NAMESPACE_BEGIN
+
+// Backward compatibility header for OpenUSD and other external
+// projects referencing files that were removed in MaterialX 1.39.5
+using SurfaceNodeHlsl = HwSurfaceNode;
+
+MATERIALX_NAMESPACE_END
+
+#endif
